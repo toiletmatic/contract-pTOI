6ea3833406bf773d6c3caa08a74915f18ed38124
Toilet Protocol Matic <88582137+toiletmatic@users.noreply.github.com>
2021-09-27 21:50:21 +0700

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,19 @@
+// 0.5.1-c8a2
+// Enable optimization
+pragma solidity ^0.5.0;
+
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
+
+/**
+ * @title pTOI
+ */
+contract Token is ERC20, ERC20Detailed {
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("Toilet Protocol", "pTOI",8) {
+        _mint(msg.sender,(2625000 * (10 ** 8)));
+    }
+}
